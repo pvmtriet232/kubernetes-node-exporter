770b4b708bc3ac66a35a5da0e734cde0b51448bd
triet <pvmtriet232@gmail.com>
2023-06-09 15:51:03 +0700

    add

--- eks-cluster/create-cluster.sh
+++ eks-cluster/create-cluster.sh
@@ -7,9 +7,9 @@ DEFAULT_NODES="1"
 
 # Assign vaues from command-line arguments, if provided
 CLUSTER_NAME=${1:-$DEFAULT_CLUSTER_NAME}
-REGION=${1:-$DEFAULT_REGION}
-NODE_TYPE=${1:-$DEFAULT_NODE_TYPE}
-NODES=${1:-$DEFAULT_NODES}
+REGION=${2:-$DEFAULT_REGION}
+NODE_TYPE=${3:-$DEFAULT_NODE_TYPE}
+NODES=${4:-$DEFAULT_NODES}
 
 
 eksctl create cluster \
